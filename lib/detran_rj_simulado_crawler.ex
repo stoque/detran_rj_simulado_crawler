26c28f3b15ff268a78966711f07a782505cf9d9b
Lucas Stoque <stoque@live.com>
2022-02-07 09:45:20 -0300

    first commit

---
+++ lib/detran_rj_simulado_crawler.ex
@@ -0,0 +1,18 @@
+defmodule DetranRjSimuladoCrawler do
+  @moduledoc """
+  Documentation for `DetranRjSimuladoCrawler`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DetranRjSimuladoCrawler.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/detran_rj_simulado_crawler_test.exs
@@ -0,0 +1,8 @@
+defmodule DetranRjSimuladoCrawlerTest do
+  use ExUnit.Case
+  doctest DetranRjSimuladoCrawler
+
+  test "greets the world" do
+    assert DetranRjSimuladoCrawler.hello() == :world
+  end
+end
